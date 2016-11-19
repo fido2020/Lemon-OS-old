8e43ac6c68eccde1573fd65dc2030eed89bcf191
JJ Roberts-White <fidojj@hotmail.com>
2016-11-19 17:18:45 +1100

    test

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lemon-OS
+# Lemon-OS
---
+++ cygwinhere.bat
@@ -0,0 +1,2 @@
+cd "%~dp0%"
+C:\cygwin\bin\mintty.exe -i /Cygwin-Terminal.ico C:\cygwin\bin\bash.exe  -l -c "cd C:/Users/fidoj/OneDrive/Documents/Lemon ; exec bash"
