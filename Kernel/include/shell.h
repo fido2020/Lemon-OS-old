5c009af52f680dd2d61571060d3b385a57ad926a
JJ Roberts-White <fidojj@hotmail.com>
2016-11-20 11:01:41 +1100

    Found more files to remove. Added keyboard input

--- Kernel/include/shell.h
+++ Kernel/include/shell.h
@@ -1,5 +1,14 @@
 #pragma once
 
+bool run;
+
 struct ShellFunction{
 	void (*func)(char* args[]);
 };
+
+extern void RunShell(){
+	run = true;
+	while(run){
+		
+	}
+}
--- Kernel/include/vbe.h
+++
@@ -1,9 +0,0 @@
-#include "int32.h"
-
-void vbe_set_mode (int mode) {
-   /* call BIOS */
-   regs16_t in, out;
-   in.eax.val = 0x4F02;
-   in.ebx.val = mode;
-   io_services (0x10, &in, &out);
-}
