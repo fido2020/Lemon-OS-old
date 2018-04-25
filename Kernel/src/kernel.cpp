2d9decfb80e1bbdf841a1f1bf90d403b6ab6865b
fido2020 <fidojj@hotmail.com>
2018-04-26 09:17:53 +1000

    Fixed a bunch of corrupted files

--- Kernel/src/kernel.cpp
+++ Kernel/src/kernel.cpp
@@ -12,26 +12,17 @@
 #include <memory.h>
 #include <windowmanager.h>
 #include <math.h>
-<<<<<<< HEAD
 #include <physallocator.h>
-=======
->>>>>>> 17e9ca9a679e395e7e3bc93ec5eb2a2a0cd4790c
 #include <bitmap.h>
 
 extern "C"
 void kmain(uint32_t mb_info_addr){
-<<<<<<< HEAD
 	multiboot_info_t mb_info;
 
 	// Initialize serial port for debugging
 	init_serial();
 	write_serial_string("Initializing Lemon...\r\n");
 
-=======
-	 
-	multiboot_info_t mb_info;
-
->>>>>>> 17e9ca9a679e395e7e3bc93ec5eb2a2a0cd4790c
 	// Initialize GDT and IDT
 	gdt_initialize();
 	idt_initialize();
@@ -44,7 +35,6 @@ void kmain(uint32_t mb_info_addr){
 	// Initialize Paging
 	paging_initialize();
 
-<<<<<<< HEAD
 	map_page(mb_info.mmapAddr, mb_info.mmapAddr, mb_info.mmapLength / PAGE_SIZE + 1);
 	multiboot_memory_map_t* memory_map = (multiboot_memory_map_t*)mb_info.mmapAddr;
 
@@ -61,26 +51,6 @@ void kmain(uint32_t mb_info_addr){
 	uint32_t vid_mem_size = mb_info.framebufferHeight*mb_info.framebufferPitch;
 
 	map_page(mb_info.framebufferAddr, mb_info.framebufferAddr, vid_mem_size / PAGE_SIZE + 1);
-=======
-	// Initialize serial port for debugging
-	init_serial();
-	write_serial_string("Initializing Lemon...\r\n"); 
-
-	map_page(mb_info.mmapAddr, mb_info.mmapAddr, mb_info.mmapLength / PAGE_SIZE + 1);
-	multiboot_memory_map_t* memory_map = (multiboot_memory_map_t*)mb_info.mmapAddr;
-
-
-	for (int i = 0; i < mb_info.mmapLength; i += memory_map->size + sizeof(memory_map->size)) {
-		memory_map += i;
-		if (memory_map->type != 1) {
-			mark_pages_reserved(memory_map->base, memory_map->base, memory_map->length / PAGE_SIZE + 1);
-		}
-	}
-
-	// Map Video Memory
-	uint32_t vid_mem_size = mb_info.framebufferHeight*mb_info.framebufferPitch;
-	map_page(mb_info.framebufferAddr, mb_info.framebufferAddr, vid_mem_size/PAGE_SIZE + 1);
->>>>>>> 17e9ca9a679e395e7e3bc93ec5eb2a2a0cd4790c
 
 	// Initialize Video Mode structure
 	video_mode_t video_mode;
@@ -108,7 +78,6 @@ void kmain(uint32_t mb_info_addr){
 	lemoninitfs_node_t** initrd_fs_nodes = initrd_list();
 	lemoninitfs_header_t* initrd_fs_header = initrd_get_header();
 
-<<<<<<< HEAD
 	bitmap_file_header_t* bmpfileheader = (bitmap_file_header_t*)(initrd_fs_nodes[0]->offset + initrd_module.mod_start);
 	bitmap_info_header_t* bmpinfoheader = (bitmap_info_header_t*)(initrd_fs_nodes[0]->offset + 14 + initrd_module.mod_start);
 
@@ -124,33 +93,6 @@ void kmain(uint32_t mb_info_addr){
 
 	// Initialize Window Manager
 	WindowManager wman(&video_mode);
-=======
-	/*for (int i = 0; i < 16; i++) {
-		lemoninitfs_node_t node = (*initrd_fs_nodes)[i];
-		char* fname = (char*)malloc(10);
-		substr(9, node.filename, fname);
-		if (fname == "Lemon.bmp") {
-			uint32_t buffer = (uint32_t)malloc(node.size+1);
-			initrd_read(node, (uint8_t*)buffer);
-			bitmap_header_t* hdr = (bitmap_header_t*)buffer;
-			write_serial_string(hdr->magic);
-			free((void*)buffer);
-		}
-		free(fname);
-	}*/
-
-	// Initialize Window Manager
-	WindowManager wman(&video_mode);
-
-	// Initialize Console
-	/*Console console(video_mode,0,0,mb_info.framebufferWidth,mb_info.framebufferHeight);
-
-	console.clear(0, 0, 0);
-
-	console.puts("RAM: ");
-	console.puts(itoa((mb_info.memoryLo + mb_info.memoryHi) / 1024));
-	console.puts(" MB\n\n");*/
->>>>>>> 17e9ca9a679e395e7e3bc93ec5eb2a2a0cd4790c
 
 	for (;;) {
 		//shell.Update();
