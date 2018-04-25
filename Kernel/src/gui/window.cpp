93cb67a30fae1b95a6ba17bae0512e38eb7502e4
fido2020 <fidojj@hotmail.com>
2018-04-26 09:23:48 +1000

    Cleanup

--- Kernel/src/gui/window.cpp
+++ Kernel/src/gui/window.cpp
@@ -9,24 +9,6 @@ uint8_t r;
 uint8_t g;
 uint8_t b;
 
-<<<<<<< HEAD
-=======
-/*Window::Window(uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
-	this->x = x;
-	this->y = y;
-	this->width = width;
-	this->height = height;
-}
-
-void Window::draw() {
-	r = random();
-	g = random();
-	b = random();
-
-	screen_fillrect(x, y, width, height, r, g, b);
-}*/
-
->>>>>>> 17e9ca9a679e395e7e3bc93ec5eb2a2a0cd4790c
 Window* Window_new(uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
 	Window* win = (Window*)malloc(sizeof(Window));
 	win->x = x;
